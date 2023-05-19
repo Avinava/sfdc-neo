a1c5e780878561cdb8106684346237bd30a2845a
avi <avidev9@gmail.com>
2023-05-19 15:52:02 +0530

    move to es

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,40 @@
-const express = require('express');
-const os = require('os');
+import * as dotenv from "dotenv";
+import session from "express-session";
+import path from "path";
+import { default as cors, default as express } from "express";
+import { errors } from "celebrate";
+import { createClient } from "redis";
+import RedisStore from "connect-redis";
+
+dotenv.config();
+
+const redisClient = createClient();
+redisClient.connect().catch(console.error);
+
+// Initialize store.
+const redisStore = new RedisStore({
+  client: redisClient,
+  prefix: "myapp:",
+});
 
 const app = express();
 
-app.use(express.static('dist'));
-app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+app.use(cors());
+app.use(express.json());
+app.use(express.static("dist"));
+app.use(errors());
 
-app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
+const sessionMiddleware = session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  store: redisStore,
+});
+
+app.use(sessionMiddleware);
+
+// need this for react router to work
+app.get("*", (req, res) => res.sendFile(path.resolve("dist", "index.html")));
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port ${process.env.PORT}`);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,61 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+import path from "path";
+import * as HtmlWebpackPlugin from "html-webpack-plugin";
+import { CleanWebpackPlugin } from "clean-webpack-plugin";
+import Dotenv from "dotenv-webpack";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const outputDirectory = 'dist';
-
-module.exports = {
-  entry: ['babel-polyfill', './src/client/index.js'],
+const outputDirectory = "dist";
+export default {
+  entry: ["babel-polyfill", "./src/client/index.js"],
   output: {
     path: path.join(__dirname, outputDirectory),
-    filename: 'bundle.js'
+    filename: "bundle.js",
   },
   module: {
-    rules: [{
+    rules: [
+      {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
-        }
+          loader: "babel-loader",
+        },
+        resolve: {
+          fullySpecified: false,
+        },
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-        loader: 'url-loader'
-      }
-    ]
+        loader: "url-loader",
+        options: {
+          limit: 100000,
+        },
+      },
+    ],
   },
   resolve: {
-    extensions: ['*', '.js', '.jsx']
+    extensions: ["*", ".js", ".jsx"],
   },
   devServer: {
     port: 3000,
     open: true,
     historyApiFallback: true,
     proxy: {
-      '/api': 'http://localhost:8080'
-    }
+      "/api/*": "http://localhost:8080",
+      "/auth/*": "http://localhost:8080",
+    },
   },
   plugins: [
-    new CleanWebpackPlugin({
-      cleanAfterEveryBuildPatterns: [outputDirectory]
-  }),
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-      favicon: './public/favicon.ico'
-    })
-  ]
+    new CleanWebpackPlugin({ cleanAfterEveryBuildPatterns: [outputDirectory] }),
+    new Dotenv(),
+    new HtmlWebpackPlugin.default({
+      template: "./public/index.html",
+      favicon: "./public/favicon.ico",
+    }),
+  ],
 };
