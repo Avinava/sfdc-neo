e42c2055f612f0281e96752180282a49828a3b60
avi <avidev9@gmail.com>
2023-06-06 22:03:57 +0530

    add temp error handling

--- src/server/routes/v1/salesforce.route.js
+++ src/server/routes/v1/salesforce.route.js
@@ -4,69 +4,64 @@ import Salesforce from "../../services/salesforce.js";
 const router = express.Router();
 
 router.get("/apexclass", async (req, res) => {
-  const salesforce = new Salesforce(req.session);
-  if (!salesforce.isVaild()) {
-    return res.status(401).send({
-      message: "You are not logged in.",
-    });
-  } else {
+  const salesforce = getSalesforceConnection(req, res);
+  if (salesforce) {
     res.send(await salesforce.getApexClasses());
   }
 });
 
 router.get("/emailtemplate", async (req, res) => {
-  const salesforce = new Salesforce(req.session);
-  if (!salesforce.isVaild()) {
-    return res.status(401).send({
-      message: "You are not logged in.",
-    });
-  } else {
+  const salesforce = getSalesforceConnection(req, res);
+  if (salesforce) {
     res.send(await salesforce.getEmailTemplates());
   }
 });
 
 router.get("/validationrule", async (req, res) => {
-  const salesforce = new Salesforce(req.session);
-  if (!salesforce.isVaild()) {
-    return res.status(401).send({
-      message: "You are not logged in.",
-    });
-  } else {
-    res.send(await salesforce.getValidationRule());
+  const salesforce = getSalesforceConnection(req, res);
+  if (salesforce) {
+    res.send(await salesforce.getValidationRules());
   }
 });
 
 router.get("/validationrule/:id", async (req, res) => {
-  const salesforce = new Salesforce(req.session);
-  if (!salesforce.isVaild()) {
-    return res.status(401).send({
-      message: "You are not logged in.",
-    });
-  } else {
+  const salesforce = getSalesforceConnection(req, res);
+  if (salesforce) {
     res.send(await salesforce.getValidationRuleMetadata(req.params.id));
   }
 });
 
 router.post("/deployclass", async (req, res) => {
-  const salesforce = new Salesforce(req.session);
-  if (!salesforce.isVaild()) {
-    return res.status(401).send({
-      message: "You are not logged in.",
-    });
-  } else {
+  const salesforce = getSalesforceConnection(req, res);
+  if (salesforce) {
     res.send(await salesforce.deployClass(req.body));
   }
 });
 
 router.get("/deployclass/:id", async (req, res) => {
-  const salesforce = new Salesforce(req.session);
-  if (!salesforce.isVaild()) {
-    return res.status(401).send({
-      message: "You are not logged in.",
-    });
-  } else {
+  const salesforce = getSalesforceConnection(req, res);
+  if (salesforce) {
     res.send(await salesforce.checkDeployStatus(req.params.id, true));
   }
 });
+
+function getSalesforceConnection(req, res) {
+  let salesforce;
+  try {
+    salesforce = new Salesforce(req.session);
+    if (!salesforce.isVaild()) {
+      salesforce = null;
+      return res.status(401).send({
+        message: "You are not logged in.",
+      });
+    }
+  } catch (exception) {
+    res.status(500).send({
+      success: false,
+      message: exception.message,
+    });
+  }
+  return salesforce;
+}
 
 export default router;
