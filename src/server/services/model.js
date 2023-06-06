10067c91890ce84d24b5bfe305c3f8adadec6b4d
avi <avidev9@gmail.com>
2023-06-06 21:36:51 +0530

    convert to number

--- src/server/services/model.js
+++ src/server/services/model.js
@@ -4,8 +4,8 @@ dotenv.config();
 
 const model = new OpenAI({
   openAIApiKey: process.env.OPENAI_API_KEY,
-  temperature: process.env.OPENAI_TEMPERATURE || 0.3,
-  maxTokens: process.env.OPENAI_MAX_TOKENS || 3000,
+  temperature: (process.env.OPENAI_TEMPERATURE || 0.3) * 1,
+  maxTokens: (process.env.OPENAI_MAX_TOKENS || 3000) * 1,
   modelName: process.env.OPENAI_MODEL_NAME || "gpt-3.5-turbo",
 });
 
