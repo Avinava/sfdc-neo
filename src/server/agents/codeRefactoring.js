05aea124701ec64df62aba8ec1f1ad97e653efe9
avi <avidev9@gmail.com>
2023-08-03 11:28:59 +0530

    refactor "scanner"

--- src/server/agents/codeRefactoring.js
+++ src/server/agents/codeRefactoring.js
@@ -1,14 +1,18 @@
 import { PromptTemplate } from "langchain/prompts";
 import { model } from "../services/model.js";
+import sfdxScanner from "../services/sfdxScanner.js";
 
 class CodeRefactoring {
   promptTemplate = `
   # YOUR TASK
-  You are a Salesforce developer who is tasked to optimize and refactor the provided class keeping in mind
-  salesforce best practices.\
+  - You are a Salesforce developer who is tasked to optimize and refactor the provided class keeping in mind salesforce best practices.
+  - Use the PMD result JSON to find the issues and refactor the code.
 
   # APEX CLASS
   {Body}
+
+  # PMD SCAN RESULTS
+  {PMDScanResults}
 
   # RESPONSE Apex Class
   `;
@@ -18,11 +22,13 @@ class CodeRefactoring {
   constructor() {
     this.prompt = new PromptTemplate({
       template: this.promptTemplate,
-      inputVariables: ["Body"],
+      inputVariables: ["Body", "PMDScanResults"],
     });
   }
 
   async generate(cls) {
+    const results = await sfdxScanner.getScanResults(cls);
+    cls.PMDScanResults = JSON.stringify(results);
     const input = await this.prompt.format(cls);
     const response = await model.call(input);
     return response;
