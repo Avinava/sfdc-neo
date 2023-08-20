56deea67344d3b7f6afce92735a7a3dcdfb99e6e
avi <avidev9@gmail.com>
2023-08-20 19:41:39 +0530

    update refactoring prompt

--- src/server/agents/codeRefactoring.js
+++ src/server/agents/codeRefactoring.js
@@ -7,7 +7,9 @@ class CodeRefactoring {
   promptTemplate = `
 # YOUR TASK
 - You are a Salesforce developer who is tasked to optimize and refactor the provided class keeping in mind salesforce best practices.
-- Use the PMD result JSON to guide you in refactoring the code.
+- Use the PMD result to guide you in refactoring the code.
+- Break the large method into smaller methods so that it is easier to read and maintain.
+- Add comments and apex-doc to the class / method if they are missing.
 
 # APEX CLASS
 {Body}
