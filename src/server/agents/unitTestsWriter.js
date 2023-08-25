4e07830bc99879164c5b3c550d50878f45e95c16
avi <avidev9@gmail.com>
2023-08-25 12:34:48 +0530

    better test prompt

--- src/server/agents/unitTestsWriter.js
+++ src/server/agents/unitTestsWriter.js
@@ -4,16 +4,14 @@ import { model } from "../services/model.js";
 class UnitTestsWriter {
   promptTemplate = `
 # YOUR TASK
-You are a developer who is writing unit test class for the provided apex class. Use the provided apex class to generate unit test class 
+You are a world class developer who is writing unit test class for the provided apex class. Use the provided apex class to generate unit test class 
 by following the below guidelines.
-- generated test class should have private and should have apex-doc comments for each method and class
+- generated test class should be private, shouldn't have hardcoded ids, have apex-doc comments (@description) for each method and class
 - use the SObject metadata (required fields, type, length) to guide you in generating test and referenced data. In test data make
-sure you populate all required fields and use the correct data type and length.
-- don't insert custom metadata types (objects ending with __mdt) in test classes
-- don't use hardcoded ids in test classes
-- use @testSetup, Asserts, System.runAs wherever possible
-- use Test.startTest() and Test.stopTest() wherever possible
-- add both positive and negative test cases wherever possible
+sure you populate all required fields and use the correct data type and length. Use @testSetup to create test data.
+- custom metadata types, objects ending with __mdt cannot be created in test classes, don't create them
+- To ensure proper testing use Asserts, System.runAs, Test.startTest() and Test.stopTest() where applicable
+- test methods should test both positive and negative scenarios and some edge cases
 - add test cases for bulk data wherever possible
 
 
