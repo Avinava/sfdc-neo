0c60d8842c2bc31557c8abe0826e40357c6bbb67
avi <avidev9@gmail.com>
2023-12-28 11:41:44 +0530

    update prompt

--- src/server/agents/codeComments.js
+++ src/server/agents/codeComments.js
@@ -4,27 +4,28 @@ import { model } from "../services/model.js";
 class CodeComments {
   promptTemplate = `
 # YOUR TASK
-You are a Salesforce developer updating an apex class to add comments and apex-doc.
+You are a Salesforce developer updating an existing APEX CLASS to add comments and apex-doc for better readability and maintenance.
+While updating the APEX CLASS make sure to follow the following guidelines.
 - add apex-doc to the class / method if they are missing
-- use the existing comments, apex-docs and make improvements where you can so that the code is easier to understand and maintain
+- use the existing comments, apex-docs and make improvements where you can so that the code is easier to understand and maintain.
 - fix any typos, spelling or grammar issues in the comments and docs
 
 # Example Apex-Doc
 - Class apex-doc
-  @description: class description
-  @author: leave blank
-  @group: leave blank
+  @description: class description <updated short description of class>
+  @author: <leave blank/unchanged>
+  @group: <leave blank/unchanged>
 
 - Method apex-doc
-  @description: method description
-  @param: parameter description
-  @return: return description
+  @description: method description <what it does>
+  @param: parameter description <type and name>
+  @return: return description <type and name>
 
 # APEX CLASS
 {Body}
 
 # RESPONSE INSTRUCTIONS
-return the updated code, don't return anything extra.
+return the updated code only.
 
 # UPDATED APEX CLASS
   
