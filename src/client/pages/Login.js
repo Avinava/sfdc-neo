0016528ae36e1f53151efd9b0de2343ec11ac700
avi <avidev9@gmail.com>
2024-01-06 23:11:44 +0530

    redesign login page

--- src/client/pages/Login.js
+++ src/client/pages/Login.js
@@ -13,22 +13,101 @@ import {
 
 class Login extends React.Component {
   render() {
+    const features = [
+      {
+        label: "Apex",
+        details: [
+          "Document code",
+          "Generating test classes",
+          "Adding comments to code",
+          "Reviewing and refactoring code",
+        ],
+      },
+      {
+        label: "Email Templates",
+        details: ["Better formatting email templates"],
+      },
+      { label: "Validation Rules", details: ["Documenting validation rules"] },
+      {
+        label: "Flow",
+        details: ["Document Flows", "Generate test classes for Flows"],
+      },
+    ];
+
     return (
       <React.Fragment>
         <Container maxWidth="xl" component="main" sx={{ pt: 5 }}>
-          <Grid
-            container
-            spacing={2}
-            justifyContent="center"
-            alignItems="center"
-          >
-            <Grid item xs={8} sm={6} lg={5} md={5}>
-              <Card>
+          <Grid container justifyContent="center" alignItems="stretch">
+            <Grid item xs={12} sm={6} lg={6} md={6}>
+              <Card
+                sx={{
+                  backgroundColor: "#f5f5f5",
+                  margin: "1rem",
+                  padding: "1rem",
+                  height: "95%",
+                }}
+              >
+                <Typography
+                  variant="h5"
+                  sx={{
+                    fontWeight: 600,
+                    marginBottom: 1.5,
+                    textAlign: "center",
+                  }}
+                >
+                  Welcome to sfdc-neo 👋🏻
+                </Typography>
+                <Typography
+                  variant="body2"
+                  component="div"
+                  sx={{ textAlign: "center" }}
+                >
+                  SFDC Neo is a Node.js application that uses OpenAI to help
+                  Salesforce developers with a variety of tasks, including:
+                </Typography>
+                <Grid container spacing={2} sx={{ mt: 2 }}>
+                  {features.map((feature, index) => (
+                    <Grid item xs={12} sm={12} md={12} key={index}>
+                      <Card>
+                        <CardContent>
+                          <Typography variant="body1" component="div">
+                            <strong>{feature.label}</strong>
+                          </Typography>
+                          {feature.details.map((detail, index) => (
+                            <Box
+                              sx={{
+                                display: "flex",
+                                alignItems: "center",
+                              }}
+                            >
+                              <Typography variant="body2">{detail}</Typography>
+                            </Box>
+                          ))}
+                        </CardContent>
+                      </Card>
+                    </Grid>
+                  ))}
+                </Grid>
+              </Card>
+            </Grid>
+            <Grid item xs={12} sm={6} lg={5} md={5}>
+              <Card
+                sx={{
+                  backgroundColor: "#e0e0e0",
+                  margin: "1rem",
+                  padding: "1rem",
+                  height: "95%",
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
                 <div style={{ background: "gray", minHeight: "4px" }}></div>
                 <CardContent sx={{ pt: 1 }}>
                   <Box
                     sx={{
-                      mb: 8,
+                      mb: 4,
                       display: "flex",
                       alignItems: "center",
                       justifyContent: "center",
@@ -36,115 +115,54 @@ class Login extends React.Component {
                     }}
                   >
                     <Typography
-                      variant="h6"
-                      sx={{
-                        ml: 3,
-                        lineHeight: 1,
-                        fontWeight: 600,
-                        textTransform: "uppercase",
-                        fontSize: "1.5rem !important",
-                        textAlign: "center",
-                      }}
-                    >
-                      sfdc-neo
-                    </Typography>
-                  </Box>
-                  <Box sx={{ mb: 2 }}>
-                    <Typography
-                      variant="h5"
-                      sx={{
-                        fontWeight: 600,
-                        marginBottom: 1.5,
-                        textAlign: "center",
-                      }}
-                    >
-                      Welcome to sfdc-neo 👋🏻
-                    </Typography>
-                    <Typography
                       variant="body2"
                       component="div"
-                      sx={{
-                        textAlign: "center",
-                      }}
+                      sx={{ textAlign: "center" }}
                     >
-                      SFDC Neo is a Node.js application that uses OpenAI to help
-                      Salesforce developers with a variety of tasks, including:
+                      Get started by logging in using your Salesforce Account
                     </Typography>
-                    <Typography
-                      variant="body2"
-                      sx={{ textAlign: "center", mt: 1 }}
-                      component="div"
-                    >
-                      <ul className="feature-list">
-                        <li>
-                          <b>Apex:</b> Document code
-                        </li>
-                        <li>
-                          <b>Apex:</b> Generating test classes
-                        </li>
-                        <li>
-                          <b>Apex:</b> Adding comments to code
-                        </li>
-                        <li>
-                          <b>Apex:</b> Reviewing and refactoring code
-                        </li>
-                        <li>
-                          <b>Email Templates:</b> Better formatting email
-                          templates
-                        </li>
-                        <li>
-                          <b>Validation Rules:</b> Documenting validation rules
-                        </li>
-                        <li>
-                          <b>Flow:</b> Document Flows
-                        </li>
-                        <li>
-                          <b>Flow:</b> Generate test classes for Flows
-                        </li>
-                      </ul>
-                    </Typography>
-                    <Alert severity="info" color="info" sx={{ mt: 2 }}>
-                      it's important to acknowledge that sfdc-neo's performance
-                      is dependent on the capabilities of OpenAI. Occasionally,
-                      there may be instances where the generated output may be
-                      inaccurate or inconsistent.
-                    </Alert>
                   </Box>
+                  <CardActions sx={{ p: 4 }}>
+                    <Grid container justifyContent="center">
+                      <Grid item xs={12}>
+                        <Button
+                          variant="contained"
+                          color="primary"
+                          fullWidth
+                          href="/api/v1/auth/production"
+                        >
+                          Login with Salesforce Production
+                        </Button>
+                      </Grid>
+                      <Grid item xs={12} sx={{ mt: 1 }}>
+                        <Button
+                          variant="contained"
+                          color="primary"
+                          fullWidth
+                          href="/api/v1/auth/production"
+                        >
+                          Login with Salesforce Developer
+                        </Button>
+                      </Grid>
+                      <Grid item xs={12} sx={{ mt: 1 }}>
+                        <Button
+                          variant="contained"
+                          color="primary"
+                          fullWidth
+                          href="/api/v1/auth/sandbox"
+                        >
+                          Login with Salesforce Sandbox
+                        </Button>
+                      </Grid>
+                    </Grid>
+                  </CardActions>
+                  <Alert severity="info" color="info" sx={{ mt: 2 }}>
+                    it's important to acknowledge that sfdc-neo's performance is
+                    dependent on the capabilities of OpenAI. Occasionally, there
+                    may be instances where the generated output may be
+                    inaccurate or inconsistent.
+                  </Alert>
                 </CardContent>
-                <CardActions sx={{ p: 3 }}>
-                  <Grid container justifyContent="center">
-                    <Grid item xs={12}>
-                      <Button
-                        variant="contained"
-                        color="primary"
-                        fullWidth
-                        href="/api/v1/auth/production"
-                      >
-                        Login with Salesforce Production
-                      </Button>
-                    </Grid>
-                    <Grid item xs={12} sx={{ mt: 1 }}>
-                      <Button
-                        variant="contained"
-                        color="primary"
-                        fullWidth
-                        href="/api/v1/auth/production"
-                      >
-                        Login with Salesforce Developer
-                      </Button>
-                    </Grid>
-                    <Grid item xs={12} sx={{ mt: 1 }}>
-                      <Button
-                        variant="contained"
-                        color="primary"
-                        fullWidth
-                        href="/api/v1/auth/sandbox"
-                      >
-                        Login with Salesforce Sandbox
-                      </Button>
-                    </Grid>
-                  </Grid>
-                </CardActions>
               </Card>
             </Grid>
           </Grid>
