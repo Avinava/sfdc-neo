c2005734e59af427fb3ae98cdaee749a9f73b6d5
avi <avidev9@gmail.com>
2024-01-07 02:21:02 +0530

    add jsdoc

--- src/server/services/salesforce/dependencyParser.js
+++ src/server/services/salesforce/dependencyParser.js
@@ -1,6 +1,13 @@
 import sfdcSoup from "sfdc-soup";
 
+/**
+ * Class for parsing dependencies using the sfdc soup
+ */
 class DependencyParser {
+  /**
+   * Create a DependencyParser.
+   * @param {Object} jsForceConnection - The jsForce connection object.
+   */
   constructor(jsForceConnection) {
     this.connection = {
       token: jsForceConnection.accessToken,
@@ -9,6 +16,11 @@ class DependencyParser {
     };
   }
 
+  /**
+   * Get the dependencies for a given entry point.
+   * @param {string} entryPoint - The entry point to get dependencies for.
+   * @returns {Promise<Object>} The dependencies.
+   */
   async getDependencies(entryPoint) {
     let soupApi = sfdcSoup(this.connection, entryPoint);
     let dependencyResponse = await soupApi.getDependencies();
